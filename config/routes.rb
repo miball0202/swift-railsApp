eed4da32a49c72429bc11f19409a5624a3c7531a
masahiro <aisacart@gmail.com>
2019-04-24 21:43:19 +0900

    fix routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'memos/index'
-  get 'memos/show'
-  get 'memos/new'
-  get 'memos/create'
-  get 'memos/edit'
-  get 'memos/update'
-  get 'memos/destroy'
+
   devise_for :users
   namespace :api, default: {format: :json} do
-    resources :memos, only: :create
+    resources :memos, only: %i[create]
   end
+
+  resources :memos
+  resources :users
 end
