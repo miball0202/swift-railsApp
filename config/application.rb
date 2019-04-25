25fc54a4ce2dfa074964a1b91f1e56646302b2ad
masahiro <aisacart@gmail.com>
2019-04-25 17:47:45 +0900

    feature Create Token. Create User Record by json request

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module SwiftRailsapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
